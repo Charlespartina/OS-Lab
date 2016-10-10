ef1c4ac3975343976f823e05813b77d11f25b69e
Charlespartina <tangziyi001@gmail.com>
2016-10-09 23:02:16 -0400

    Update

--- ziyitang_lab2/lab2.cpp
+++ ziyitang_lab2/lab2.cpp
@@ -23,6 +23,7 @@ typedef long long ll;
 using namespace std;
 
 queue<ll> randomNums;
+bool verbose;
 
 // Process Class
 struct Process {
@@ -358,12 +359,126 @@ void RR(vector<Process> allProcesses, string name, int quantum){
 		timestamp++;
 	}
 }
-void UNI(vector<Process> allProcesses){
 
+// Uniprogramming
+void UNI(vector<Process> allProcesses, string name){
+	// Priority Queue for Ready Time and Process Index
+	priority_queue<pair<int,int>, vector<pair<int,int> >, greater<pair<int,int> > > readyQueue;
+	// Init SummaryData
+	SummaryData summary = SummaryData();
+	// No process is running right now
+	int cur_process = -1;
+	// Keep track of the unstarted processes
+	int pt = 0;
+	int timestamp = 0;
+	while(1){
+		// Flag indicating that every process is terminated
+		bool TERMINATED = true;
+		// Check status for all processes
+		// Make one move
+		printf("Before cycle   %d:", timestamp);
+		bool IO = false;
+		for(int i = 0; i < allProcesses.size(); i++){
+			Process* now = &allProcesses[i];
+			//cout << "sum: " << now->states[0] << " " << now->states[1] << " " << now->states[2] << endl;
+			cout << "    ";
+			// Not Created
+			if(now->cur_state == -1){
+				cout << "unstarted 0";
+				TERMINATED = false;
+				continue;
+			}
+			// Terminated
+			if(now->cur_state == 3){
+				cout << "terminated 0";
+				if(now->finish_time == -1){
+					now->finish_time = timestamp-1;
+					summary.avg_turn_time += now->finish_time-now->A_;
+				}
+				continue;
+			}
+
+			// If process is running or blocking
+			TERMINATED = false;
+			int cur_state = now->cur_state;
+			if(cur_state == 1){
+				cout << "running "; cout << (now->states)[cur_state]--;
+				now->running_time++;
+				summary.cpu_util_time++; // Utilize CPU
+				if(now->states[cur_state] == 0){
+					// The current cpu burst time is reached
+					if(now->running_time == allProcesses[i].C_){
+						// Terminate this process
+						now->cur_state = 3;
+						cur_process = -1;
+					} else {
+						// Blocking
+						now->cur_state = 2;
+					}
+				}
+			} else if(cur_state == 2){
+				cout << "blocked "; cout << (now->states)[cur_state]--;
+				now->io_time++;
+				IO = true; // Some process is waiting for IO
+				if(now->states[cur_state] == 0){
+					// Run another round
+					int cpu_burst = min(now->C_-now->running_time, randomMod_feed(now->B_));
+					int io_burst = now->M_ * cpu_burst;
+					now->states[1] = cpu_burst;
+					now->states[2] = io_burst;
+					now->cur_state = 1;
+				}
+			} else{
+				// Otherwise, the process is in ready state
+				now->waiting_time++;
+				summary.avg_wait_time++;
+				cout << "ready 0";
+			}
+		}
+		if(IO) summary.io_util_time++; // Utilize I/O
+
+		// All processes are terminated
+		if(TERMINATED == true) {
+			cout << endl;
+			int sz = allProcesses.size();
+			summary.gather(sz, timestamp);
+			printInfo(name, allProcesses, summary);
+			break;
+		}
+
+		// Add processes into ready list if they are created at this time
+		while(pt < allProcesses.size() && allProcesses[pt].A_ == timestamp){
+			// cout << "add" << allProcesses[pt].A_ << " " << allProcesses[pt].B_ << " " << allProcesses[pt].C_ << " " << allProcesses[pt].M_ << endl;
+			allProcesses[pt].cur_state = 0;
+			readyQueue.push(make_pair(timestamp, allProcesses[pt].idx_));
+			pt++;
+		}
+
+		// Some process is terminated
+		if(cur_process == -1){
+			// No process is ready
+			if(!readyQueue.empty()) {
+				int nowIdx = readyQueue.top().second; readyQueue.pop();
+				Process* now = &allProcesses[nowIdx];
+				// cout << now.A_ << " " << now.B_ << " " << now.C_ << " " << now.M_ << endl;
+				int cpu_burst = min(now->C_-now->running_time, randomMod_feed(now->B_));
+				int io_burst = now->M_ * cpu_burst;
+				now->states[1] = cpu_burst;
+				now->states[2] = io_burst;
+				now->cur_state = 1;
+				cur_process = nowIdx;
+			}
+		}
+		cout << endl;
+		timestamp++;
+	}
 }
+
+// Shortest Job First
 void SJF(vector<Process> allProcesses){
 
 }
+
 int main(){
 	vector<Process> allProcesses;
 	// Input the process tuples
@@ -411,6 +526,7 @@ int main(){
 	}
 	fclose(stdin);
 	// FCFS(allProcesses, "First Come First Served");
-	RR(allProcesses, "Round Robbin", 2);
+	// RR(allProcesses, "Round Robbin", 2);
+	// UNI(allProcesses, "Uniprogramming");
 	return 0;
 }
